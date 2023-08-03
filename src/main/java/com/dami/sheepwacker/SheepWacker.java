ccf3c81e22e055a06201b1340cf0ef03e1396dc7
DamiJ <damianojongerius@gmail.com>
2023-08-03 20:56:11 +0200

    implements example

--- src/main/java/com/dami/sheepwacker/SheepWacker.java
+++ src/main/java/com/dami/sheepwacker/SheepWacker.java
@@ -1,17 +1,22 @@
 package com.dami.sheepwacker;
 
+import org.bukkit.WeatherType;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class SheepWacker extends JavaPlugin {
+public final class SheepWacker extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        System.out.println("plugin werkt toch wel");
+        getServer().getPluginManager().registerEvents(this,this);
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent e){
+        System.out.println("player joined" + e.getPlayer().getName());
+        e.setJoinMessage("welcome to being gay " + e.getPlayer().getName());
     }
 }
