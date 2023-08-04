73c1d840d8b0a40a7136217518336fc61206bf35
KaGe <geenenskenshiro@gmail.com>
2023-08-04 13:43:45 +0200

    Dye and concrete linked. 1 error met een raar haakje onderaan de switchcase

--- src/main/java/com/dami/sheepwacker/Util/Positions.java
+++ src/main/java/com/dami/sheepwacker/Util/Positions.java
@@ -1,12 +1,11 @@
 package com.dami.sheepwacker.Util;
 
 import com.dami.sheepwacker.SheepWacker;
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.World;
+import org.bukkit.*;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Sheep;
+import org.bukkit.material.Wool;
 import org.bukkit.scheduler.BukkitScheduler;
 import org.jetbrains.annotations.NotNull;
 
@@ -35,6 +34,7 @@ public class Positions implements Runnable{
             Block block = world.getBlockAt(location.getBlockX(), location.getBlockY() - 1, location.getBlockZ());
             Bukkit.broadcastMessage(block.getType().toString());
             sh.getColor();
+
         }
 
         ReDo();
@@ -56,5 +56,91 @@ public class Positions implements Runnable{
     private void ReDo(){
         BukkitScheduler scheduler = main.getServer().getScheduler();
         scheduler.scheduleSyncDelayedTask(main, this, 20L);
+    }
+
+    private boolean compareColor(DyeColor sheepColor, Material blockMaterial){
+        switch (sheepColor){
+            case RED -> {
+                if(blockMaterial == Material.RED_CONCRETE){
+                return true;
+                }
+            }
+            case BLUE -> {
+                    if(blockMaterial == Material.BLUE_CONCRETE){
+                        return true;
+                    }
+
+            }
+            case CYAN -> {
+                if(blockMaterial == Material.CYAN_CONCRETE){
+                    return true;
+                }
+            }
+            case GRAY -> {
+                if(blockMaterial == Material.GRAY_CONCRETE){
+                    return true;
+                }
+            }
+            case LIME -> {
+                if(blockMaterial == Material.LIME_CONCRETE){
+                    return true;
+                }
+            }
+            case PINK -> {
+                if(blockMaterial == Material.PINK_CONCRETE){
+                    return true;
+                }
+            }
+            case BLACK -> {
+                if(blockMaterial == Material.BLACK_CONCRETE){
+                    return true;
+                }
+            }
+            case BROWN -> {
+                if(blockMaterial == Material.BROWN_CONCRETE){
+                    return true;
+                }
+            }
+            case GREEN -> {
+                if(blockMaterial == Material.GREEN_CONCRETE){
+                    return true;
+                }
+            }
+            case WHITE -> {
+                if(blockMaterial == Material.WHITE_CONCRETE){
+                    return true;
+                }
+            }
+            case ORANGE -> {
+                if(blockMaterial == Material.ORANGE_CONCRETE){
+                    return true;
+                }
+            }
+            case PURPLE -> {
+                if(blockMaterial == Material.PURPLE_CONCRETE){
+                    return true;
+                }
+            }
+            case YELLOW -> {
+                if(blockMaterial == Material.YELLOW_CONCRETE){
+                    return true;
+                }
+            }
+            case MAGENTA -> {
+                if(blockMaterial == Material.MAGENTA_CONCRETE){
+                    return true;
+                }
+            }
+            case LIGHT_BLUE -> {
+                if(blockMaterial == Material.LIGHT_BLUE_CONCRETE){
+                    return true;
+                }
+            }
+            case LIGHT_GRAY -> {
+                if(blockMaterial == Material.LIGHT_GRAY_CONCRETE){
+                    return true;
+                }
+            }
+        }
     }
 }
